1cc5ff5d1e33ec1f4e59b26896787c2d040b360d
Gareth Lloyd <garethlloyd@Gareths-MacBook-Pro-7.local>
2017-12-05 21:45:01 +0000

    Add filters component, convert to SCSS

--- map-client/src/app/app.component.ts
+++ map-client/src/app/app.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
   lat: number = 51.678;
   lng: number = 7.809;
 }
--- map-client/src/app/app.module.ts
+++ map-client/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FiltersComponent } from './filters/filters.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBpgPv4Ksu7rb1o_N1zF4N2XaEJ_k-LwQM'
-    })
+    }),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

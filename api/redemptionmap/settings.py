8cec3104273cfaa47189570c3f59554a72d66c73
Gareth Lloyd <garethlloyd@Gareths-MacBook-Pro-7.local>
2018-01-21 23:52:22 +0000

    update settings

--- api/redemptionmap/settings.py
+++ api/redemptionmap/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 
 # Password validation

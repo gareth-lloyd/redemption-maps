471300d756bd2c0c47c2fbdd4e4574f306cf8d18
Gareth Lloyd <garethlloyd@Gareths-MacBook-Pro-7.local>
2018-03-30 19:54:31 +0100

    settings

--- api/redemptionmap/prod.py
+++ api/redemptionmap/prod.py
@@ -3,5 +3,5 @@ from redemptionmap.settings import *
 
 DEBUG = False
 GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so'
-GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos.so'
+GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so'
 PROJ4_LIBRARY_PATH = "/app/.heroku/vendor/lib/libproj.so"

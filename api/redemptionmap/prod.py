996fae6e22e8b541e7f7ed711f15f4f2b4171abf
Gareth Lloyd <garethlloyd@Gareths-MacBook-Pro-7.local>
2018-03-30 17:32:01 +0100

    settings

--- api/redemptionmap/prod.py
+++ api/redemptionmap/prod.py
@@ -1,6 +1,3 @@
 from redemptionmap.settings import *
 
 DEBUG = False
-
-GEOS_LIBRARY_PATH = os.path.join(os.environ.get('GEOS_LIBRARY_PATH', ''), 'libgeos_c.so')
-GDAL_LIBRARY_PATH = os.path.join(os.environ.get('GDAL_LIBRARY_PATH', ''), 'libgdal.so')
